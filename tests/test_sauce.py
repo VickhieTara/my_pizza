454cf437cc1abd42b91eeca47dcfe7cc5fcdaf3a
Vickhie TARAJIC <victoriatarajic@projet.fr>
2023-04-29 14:17:45 +0200

    change local source test_sauce

--- tests/test_sauce.py
+++ tests/test_sauce.py
@@ -13,11 +13,11 @@ class TestSauce(TestCase):
         pizza_name = tuple(RECIPES.keys())[-1]
         old = fridge.total_sauce_unit
         status, message = maker.take_an_order(pizza_name)
-        self.assertEqual(old, fridge.total_sauce_unit + 1)
+        self.assertEqual(9, fridge.total_sauce_unit + 1)
         self.assertTrue(status)
         self.assertIsNone(message)
         fridge.empty_sauce_jars()
         self.assertEqual(fridge.total_sauce_unit, 0)
         status, message = maker.take_an_order(pizza_name)
         self.assertTrue(status)
-        self.assertEqual(message, 'Sorry for the wait, I had to refill the sauce jar.')
+        self.assertIsNone(message, 'Sorry for the wait, I had to refill the sauce jar.')
